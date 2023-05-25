60e6f7975f93cdc42ace793f23553e4f4dce36b1
Michał Michalik <86044526+michalik-michal@users.noreply.github.com>
2023-05-25 10:06:55 +0200

    Initial Commit

---
+++ looksgood/looksgoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  looksgoodApp.swift
+//  looksgood
+//
+//  Created by Michal Michalik on 25/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct looksgoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

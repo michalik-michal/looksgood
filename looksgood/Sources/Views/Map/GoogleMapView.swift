a83f047e569aa33ecf0358d94cad72ec9d685174
Michał Michalik <86044526+michalik-michal@users.noreply.github.com>
2023-05-30 04:56:33 +0200

    GoogleMaps: Get map working

--- looksgood/Sources/Views/Map/GoogleMapView.swift
+++ looksgood/Sources/Views/Map/GoogleMapView.swift
@@ -1,27 +1,63 @@
 import SwiftUI
 import GoogleMaps
 
+
+struct CustomMarker {
+    var lat: Double
+    var long: Double
+    var title: String
+}
+
 struct GoogleMapsView: UIViewRepresentable {
 
-    @EnvironmentObject var locationManager: LocationManager
-    // This will be published or binding
+    @EnvironmentObject fileprivate var locationManager: LocationManager
+    //This will be published or binding
     private let markers: [GMSMarker] = []
-
+    
+    
     private let zoom: Float = 15.0
     
-    func makeUIView(context: Self.Context) -> GMSMapView {
+    func makeCoordinator() -> Coordinator {
+            Coordinator(self)
+        }
+    
+    func makeUIView(context: Context) -> GMSMapView {
         let camera = GMSCameraPosition.camera(withLatitude: locationManager.latitude, longitude: locationManager.longitude, zoom: zoom)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         mapView.settings.myLocationButton = true
         mapView.isMyLocationEnabled = true
-        markers.forEach { marker in
-              marker.map = mapView
-            }
+        mapView.delegate = context.coordinator
+        locationManager.customMarkers.forEach { marker in
+            let mapMarker = GMSMarker(position: CLLocationCoordinate2D(latitude: marker.lat, longitude: marker.long))
+            mapMarker.userData = context.coordinator
+            mapMarker.map = mapView
+        }
         return mapView
     }
-    
+
     func updateUIView(_ mapView: GMSMapView, context: Context) {
         mapView.animate(toLocation: CLLocationCoordinate2D(latitude: locationManager.latitude, longitude: locationManager.longitude))
+    }
+}
+
+class Coordinator: NSObject, GMSMapViewDelegate {
+    let parent: GoogleMapsView
+
+    @EnvironmentObject private var locationManager: LocationManager
+
+    init(_ parent: GoogleMapsView) {
+        self.parent = parent
+    }
+
+    internal func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
+        mapView.animate(toLocation: marker.position)
+        mapView.animate(toZoom: 15.0)
+        let markers = parent.locationManager.customMarkers
+        let tappedMarker = markers.filter { $0.long == marker.position.longitude && $0.lat == marker.position.latitude }
+        print("xdd")
+        print(tappedMarker)
+
+        return true
     }
 }
 
--- looksgood/Sources/Views/Map/LocationManager.swift
+++ looksgood/Sources/Views/Map/LocationManager.swift
@@ -1,6 +1,7 @@
 import Combine
 import CoreLocation
 
+@MainActor
 class LocationManager: NSObject, ObservableObject {
     private let locationManager = CLLocationManager()
     
@@ -10,27 +11,51 @@ class LocationManager: NSObject, ObservableObject {
             objectWillChange.send()
         }
     }
+    @Published var pickedMarker: CustomMarker?
+    @Published var customMarkers: [CustomMarker] = []
+    
     
     var latitude: CLLocationDegrees {
-        return location?.coordinate.latitude ?? 0
+        return location?.coordinate.latitude ?? 50.059683
     }
     
     var longitude: CLLocationDegrees {
-        return location?.coordinate.longitude ?? 0
+        return location?.coordinate.longitude ?? 19.934544
     }
     
     override init() {
-      super.init()
+        super.init()
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
 
-      locationManager.delegate = self
-      locationManager.desiredAccuracy = kCLLocationAccuracyBest
-      locationManager.requestWhenInUseAuthorization()
-      locationManager.startUpdatingLocation()
+    private func checkLocationAuthorization() {
+        switch locationManager.authorizationStatus {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            print("Show some alert")
+        case .denied:
+            print("Show some alert")
+        case .authorizedAlways, .authorizedWhenInUse:
+            if let center = locationManager.location?.coordinate {
+                location = CLLocation(latitude: center.latitude, longitude: center.longitude)
+            }
+        @unknown default:
+            break
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuthorization()
     }
 }
 
 extension LocationManager: CLLocationManagerDelegate {
-
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if shouldUpdateLocation {
             guard let location = locations.last else { return }
